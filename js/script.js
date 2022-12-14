70b3c381d37475bac913ef5fc117314c5a0e089d
Frah01 <vinario.francesco@gmail.com>
2022-12-14 09:45:14 +0100

    Aggiustamento console.log

--- js/script.js
+++ js/script.js
@@ -37,9 +37,16 @@ let container = document.getElementById('content')
 
 for(let i=0; i<team.length; i++){
     let teams = team[i];
-    let element = `<div class="content-js card"><h4>${teams.name}</h4> <h4>${teams.role}</h4> <div><img src="${teams.image}"</div></div>`
+
+    // for(let key in team){
+    //     console.log(teams[key])
+    // }
+    
+    let element = 
+    `<div class="content-js card"><h4>${teams.name}</h4>
+     <h4>${teams.role}</h4> <div><img src="${teams.image}"</div></div>`
     container.innerHTML += element
-    console.log(`${teams.name} ${teams.role} ${teams.image}`)
+    console.log(`${teams.name} - ${teams.role} - ${teams.image}`)
 }
 
 
